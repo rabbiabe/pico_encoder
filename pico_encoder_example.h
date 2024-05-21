3e02871d1acc831909b829ddee237fb8765350b2
rabbiabe <asfriedman@gmail.com>
2024-05-21 10:56:33 -0400

    Update pico_encoder_example.h

--- pico_encoder_example.h
+++ pico_encoder_example.h
@@ -8,10 +8,6 @@ const uint8_t   pinEncCW = 20,  // pin 26
                 pinEncCCW = 21, // pin 27
                 pinEncSW = 22;  // pin 29
 
-const uint32_t input_mask = (1 << pinEncCCW) | 
-                            (1 << pinEncCW) | 
-                            (1 << pinEncSW);
-
 int32_t counter;
 
 Rotary encoder(&encoderCallback, pinEncCW, pinEncCCW, pinEncSW);
